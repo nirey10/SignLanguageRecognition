131f9322fbbdcca449c0e60190d54d3273098977
nir <nirey10@gmail.com>
2016-04-25 18:42:11 +0300

    kkk4

--- SignLanguageRecognition/SignLanguageRecognition/CountinuousTranslate.Designer.cs
+++ SignLanguageRecognition/SignLanguageRecognition/CountinuousTranslate.Designer.cs
@@ -11,15 +11,15 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-            System.Environment.Exit(0);
-        }
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing && (components != null))
+        //    {
+        //        components.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //    System.Environment.Exit(0);
+        //}
 
         #region Windows Form Designer generated code
 
@@ -30,7 +30,7 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CountinuousTranslate));
-            this.button3 = new System.Windows.Forms.Button();
+            this.BackToMenuBtn = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.StartListeningBtn = new System.Windows.Forms.Button();
             this.StopListeningBtn = new System.Windows.Forms.Button();
@@ -52,18 +52,17 @@
             ((System.ComponentModel.ISupportInitialize)(this.StatusGif)).BeginInit();
             this.SuspendLayout();
             // 
-            // button3
+            // BackToMenuBtn
             // 
-            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
-            this.button3.ForeColor = System.Drawing.Color.SteelBlue;
-            this.button3.Location = new System.Drawing.Point(29, 458);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(156, 39);
-            this.button3.TabIndex = 20;
-            this.button3.Text = "Back to Menu";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.BackToMenuBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.BackToMenuBtn.ForeColor = System.Drawing.Color.SteelBlue;
+            this.BackToMenuBtn.Location = new System.Drawing.Point(22, 372);
+            this.BackToMenuBtn.Name = "BackToMenuBtn";
+            this.BackToMenuBtn.Size = new System.Drawing.Size(117, 32);
+            this.BackToMenuBtn.TabIndex = 20;
+            this.BackToMenuBtn.Text = "Back to Menu";
+            this.BackToMenuBtn.UseVisualStyleBackColor = true;
+            this.BackToMenuBtn.Click += new System.EventHandler(this.BackToMenuBtn_Click);
             // 
             // label2
             // 
@@ -71,10 +70,9 @@
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Monotype Corsiva", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.SteelBlue;
-            this.label2.Location = new System.Drawing.Point(189, 36);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(142, 29);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(353, 45);
+            this.label2.Size = new System.Drawing.Size(273, 36);
             this.label2.TabIndex = 21;
             this.label2.Text = "Continuous Translation";
             this.label2.Click += new System.EventHandler(this.label2_Click);
@@ -86,10 +84,9 @@
             this.StartListeningBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.StartListeningBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.StartListeningBtn.ForeColor = System.Drawing.Color.Transparent;
-            this.StartListeningBtn.Location = new System.Drawing.Point(459, 436);
-            this.StartListeningBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.StartListeningBtn.Location = new System.Drawing.Point(344, 354);
             this.StartListeningBtn.Name = "StartListeningBtn";
-            this.StartListeningBtn.Size = new System.Drawing.Size(67, 62);
+            this.StartListeningBtn.Size = new System.Drawing.Size(50, 50);
             this.StartListeningBtn.TabIndex = 22;
             this.StartListeningBtn.TextAlign = System.Drawing.ContentAlignment.TopRight;
             this.StartListeningBtn.UseVisualStyleBackColor = false;
@@ -102,10 +99,9 @@
             this.StopListeningBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.StopListeningBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.StopListeningBtn.ForeColor = System.Drawing.Color.Transparent;
-            this.StopListeningBtn.Location = new System.Drawing.Point(613, 436);
-            this.StopListeningBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.StopListeningBtn.Location = new System.Drawing.Point(460, 354);
             this.StopListeningBtn.Name = "StopListeningBtn";
-            this.StopListeningBtn.Size = new System.Drawing.Size(67, 62);
+            this.StopListeningBtn.Size = new System.Drawing.Size(50, 50);
             this.StopListeningBtn.TabIndex = 23;
             this.StopListeningBtn.TextAlign = System.Drawing.ContentAlignment.TopRight;
             this.StopListeningBtn.UseVisualStyleBackColor = false;
@@ -115,11 +111,10 @@
             // 
             this.output.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.output.ForeColor = System.Drawing.Color.Salmon;
-            this.output.Location = new System.Drawing.Point(412, 140);
-            this.output.Margin = new System.Windows.Forms.Padding(4);
+            this.output.Location = new System.Drawing.Point(309, 114);
             this.output.Name = "output";
             this.output.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.output.Size = new System.Drawing.Size(303, 180);
+            this.output.Size = new System.Drawing.Size(228, 147);
             this.output.TabIndex = 24;
             this.output.Text = "";
             // 
@@ -139,11 +134,9 @@
             this.informationBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.informationBox.Font = new System.Drawing.Font("Monotype Corsiva", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.informationBox.ForeColor = System.Drawing.Color.SteelBlue;
-            this.informationBox.Location = new System.Drawing.Point(16, 122);
-            this.informationBox.Margin = new System.Windows.Forms.Padding(4);
+            this.informationBox.Location = new System.Drawing.Point(12, 99);
             this.informationBox.Name = "informationBox";
-            this.informationBox.Padding = new System.Windows.Forms.Padding(4);
-            this.informationBox.Size = new System.Drawing.Size(311, 230);
+            this.informationBox.Size = new System.Drawing.Size(233, 187);
             this.informationBox.TabIndex = 25;
             this.informationBox.TabStop = false;
             this.informationBox.Text = "Coordinates Information";
@@ -154,10 +147,9 @@
             this.OutputData5.BackColor = System.Drawing.Color.Transparent;
             this.OutputData5.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.OutputData5.ForeColor = System.Drawing.Color.SteelBlue;
-            this.OutputData5.Location = new System.Drawing.Point(116, 172);
-            this.OutputData5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.OutputData5.Location = new System.Drawing.Point(87, 140);
             this.OutputData5.Name = "OutputData5";
-            this.OutputData5.Size = new System.Drawing.Size(66, 28);
+            this.OutputData5.Size = new System.Drawing.Size(51, 22);
             this.OutputData5.TabIndex = 14;
             this.OutputData5.Text = "(0,0,0)";
             // 
@@ -167,10 +159,9 @@
             this.OutputData4.BackColor = System.Drawing.Color.Transparent;
             this.OutputData4.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.OutputData4.ForeColor = System.Drawing.Color.SteelBlue;
-            this.OutputData4.Location = new System.Drawing.Point(116, 145);
-            this.OutputData4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.OutputData4.Location = new System.Drawing.Point(87, 118);
             this.OutputData4.Name = "OutputData4";
-            this.OutputData4.Size = new System.Drawing.Size(66, 28);
+            this.OutputData4.Size = new System.Drawing.Size(51, 22);
             this.OutputData4.TabIndex = 13;
             this.OutputData4.Text = "(0,0,0)";
             // 
@@ -180,10 +171,9 @@
             this.OutputData3.BackColor = System.Drawing.Color.Transparent;
             this.OutputData3.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.OutputData3.ForeColor = System.Drawing.Color.SteelBlue;
-            this.OutputData3.Location = new System.Drawing.Point(116, 118);
-            this.OutputData3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.OutputData3.Location = new System.Drawing.Point(87, 96);
             this.OutputData3.Name = "OutputData3";
-            this.OutputData3.Size = new System.Drawing.Size(66, 28);
+            this.OutputData3.Size = new System.Drawing.Size(51, 22);
             this.OutputData3.TabIndex = 12;
             this.OutputData3.Text = "(0,0,0)";
             // 
@@ -193,10 +183,9 @@
             this.OutputData2.BackColor = System.Drawing.Color.Transparent;
             this.OutputData2.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.OutputData2.ForeColor = System.Drawing.Color.SteelBlue;
-            this.OutputData2.Location = new System.Drawing.Point(116, 91);
-            this.OutputData2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.OutputData2.Location = new System.Drawing.Point(87, 74);
             this.OutputData2.Name = "OutputData2";
-            this.OutputData2.Size = new System.Drawing.Size(66, 28);
+            this.OutputData2.Size = new System.Drawing.Size(51, 22);
             this.OutputData2.TabIndex = 11;
             this.OutputData2.Text = "(0,0,0)";
             // 
@@ -206,10 +195,9 @@
             this.OutputData1.BackColor = System.Drawing.Color.Transparent;
             this.OutputData1.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.OutputData1.ForeColor = System.Drawing.Color.SteelBlue;
-            this.OutputData1.Location = new System.Drawing.Point(116, 64);
-            this.OutputData1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.OutputData1.Location = new System.Drawing.Point(87, 52);
             this.OutputData1.Name = "OutputData1";
-            this.OutputData1.Size = new System.Drawing.Size(66, 28);
+            this.OutputData1.Size = new System.Drawing.Size(51, 22);
             this.OutputData1.TabIndex = 10;
             this.OutputData1.Text = "(0,0,0)";
             // 
@@ -219,10 +207,9 @@
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.Salmon;
-            this.label7.Location = new System.Drawing.Point(8, 172);
-            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Location = new System.Drawing.Point(6, 140);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(69, 28);
+            this.label7.Size = new System.Drawing.Size(55, 22);
             this.label7.TabIndex = 8;
             this.label7.Text = "Pinky:";
             // 
@@ -232,10 +219,9 @@
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.Salmon;
-            this.label6.Location = new System.Drawing.Point(8, 145);
-            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Location = new System.Drawing.Point(6, 118);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(59, 28);
+            this.label6.Size = new System.Drawing.Size(48, 22);
             this.label6.TabIndex = 7;
             this.label6.Text = "Ring:";
             // 
@@ -245,10 +231,9 @@
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Salmon;
-            this.label5.Location = new System.Drawing.Point(8, 118);
-            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Location = new System.Drawing.Point(6, 96);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(80, 28);
+            this.label5.Size = new System.Drawing.Size(64, 22);
             this.label5.TabIndex = 6;
             this.label5.Text = "Middle:";
             // 
@@ -258,10 +243,9 @@
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Salmon;
-            this.label4.Location = new System.Drawing.Point(8, 91);
-            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Location = new System.Drawing.Point(6, 74);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(66, 28);
+            this.label4.Size = new System.Drawing.Size(55, 22);
             this.label4.TabIndex = 5;
             this.label4.Text = "Index:";
             // 
@@ -271,10 +255,9 @@
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Salmon;
-            this.label3.Location = new System.Drawing.Point(8, 64);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(6, 52);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(77, 28);
+            this.label3.Size = new System.Drawing.Size(62, 22);
             this.label3.TabIndex = 4;
             this.label3.Text = "Thumb:";
             // 
@@ -283,10 +266,9 @@
             this.StatusGif.BackColor = System.Drawing.Color.Transparent;
             this.StatusGif.Enabled = false;
             this.StatusGif.Image = global::SignLanguageRecognition.Properties.Resources.Loading11;
-            this.StatusGif.Location = new System.Drawing.Point(609, 340);
-            this.StatusGif.Margin = new System.Windows.Forms.Padding(4);
+            this.StatusGif.Location = new System.Drawing.Point(457, 276);
             this.StatusGif.Name = "StatusGif";
-            this.StatusGif.Size = new System.Drawing.Size(71, 59);
+            this.StatusGif.Size = new System.Drawing.Size(53, 48);
             this.StatusGif.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.StatusGif.TabIndex = 26;
             this.StatusGif.TabStop = false;
@@ -297,19 +279,18 @@
             this.TranslationLabel.BackColor = System.Drawing.Color.Transparent;
             this.TranslationLabel.Font = new System.Drawing.Font("Monotype Corsiva", 14.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TranslationLabel.ForeColor = System.Drawing.Color.Salmon;
-            this.TranslationLabel.Location = new System.Drawing.Point(407, 358);
-            this.TranslationLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.TranslationLabel.Location = new System.Drawing.Point(305, 291);
             this.TranslationLabel.Name = "TranslationLabel";
-            this.TranslationLabel.Size = new System.Drawing.Size(178, 28);
+            this.TranslationLabel.Size = new System.Drawing.Size(143, 22);
             this.TranslationLabel.TabIndex = 27;
             this.TranslationLabel.Text = "Translation Status:";
             // 
             // CountinuousTranslate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::SignLanguageRecognition.Properties.Resources.images;
-            this.ClientSize = new System.Drawing.Size(759, 534);
+            this.ClientSize = new System.Drawing.Size(569, 434);
             this.Controls.Add(this.TranslationLabel);
             this.Controls.Add(this.StatusGif);
             this.Controls.Add(this.informationBox);
@@ -317,8 +298,7 @@
             this.Controls.Add(this.StopListeningBtn);
             this.Controls.Add(this.StartListeningBtn);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button3);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Controls.Add(this.BackToMenuBtn);
             this.Name = "CountinuousTranslate";
             this.Text = "Sign Recognition";
             this.Load += new System.EventHandler(this.CountinuousTranslate_Load);
@@ -331,7 +311,7 @@
         }
 
         #endregion
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button BackToMenuBtn;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button StartListeningBtn;
         private System.Windows.Forms.Button StopListeningBtn;
--- SignLanguageRecognition/SignLanguageRecognition/CountinuousTranslate.cs
+++ SignLanguageRecognition/SignLanguageRecognition/CountinuousTranslate.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Accord.MachineLearning.VectorMachines;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Statistics.Kernels;
+using Leap;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,34 +16,234 @@ using System.Windows.Forms;
 
 namespace SignLanguageRecognition
 {
-    public partial class CountinuousTranslate : Form
+    public partial class CountinuousTranslate : Form, ILeapEventDelegate
     {
-        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+
+        OleDbConnection connection;
+        OleDbCommand command;
+        OleDbDataReader dr;
+
+        // Leap Listener 
+        private Controller controller;
+        private LeapEventListener listener;
+        delegate void LeapEventDelegate(string EventName);
+
+
+        int numOfClasses = 0; // saves the number of calsses/letters
+        double[][] inputs;
+        // Output for each of the inputs
+        int[] outputs;
+
+        Frame currentFrame;
+        private int i = 0; // listener interval
+
+        Timer timer; // timer variables
          private static int ts=0; 
+
+
         public CountinuousTranslate()
         {
             InitializeComponent();
+            InitializeSVM();
+            InitilizeTimer();
 
+            this.controller = new Controller();
+            this.listener = new LeapEventListener(this);
+            controller.AddListener(listener);
+
+
+           
+        }
+        void InitilizeTimer()
+        {
+            timer = new Timer();
             timer.Tick += new EventHandler(Timer_Tick);
             timer.Interval = Constants.continuousTimerInterval; //10 is the default 
             timer.Start();
         }
-        	
-		private void Timer_Tick(Object sender, EventArgs e) //timer for continuous check 
+
+        void connectHandler() // set leap gestures when connected
+        {
+            this.controller.EnableGesture(Gesture.GestureType.TYPE_CIRCLE);
+            this.controller.Config.SetFloat("Gesture.Circle.MinRadius", 40.0f);
+            this.controller.EnableGesture(Gesture.GestureType.TYPE_SWIPE);
+
+
+        }
+        protected override void Dispose(bool disposing)
+        {
+            try
+            {
+                if (disposing)
+                {
+                    if (components != null)
+                    {
+                        components.Dispose();
+                    }
+                    this.controller.RemoveListener(this.listener);
+                    this.controller.Dispose();
+                }
+            }
+            finally
+            {
+                base.Dispose(disposing);
+            }
+        }
+        public void LeapEventNotification(string EventName) // event notification handler
+        {
+            if (!this.InvokeRequired)
+            {
+                switch (EventName)
+                {
+                    case "onInit":
+                        Debug.WriteLine("Init");
+                        break;
+                    case "onConnect":
+                        this.connectHandler();
+                        break;
+                    case "onDisconnect":
+                        //this.connectHandler();
+                        break;
+                    case "onFrame":
+                        if (!this.Disposing)
+                            this.frameListener(this.controller.Frame()); // sends frame to the handler
+                        break;
+                }
+            }
+            else
+            {
+                this.BeginInvoke(new LeapEventDelegate(LeapEventNotification), new object[] { EventName });
+            }
+        }
+        void frameListener(Frame frame) // the frame handler
+        {
+            currentFrame = frame;
+
+            double[] distances = new double[5];
+
+
+            i++;
+            if (i == Constants.framesInterval)
+            {
+
+
+                distances = LeapEventListener.getDistances(frame);
+                OutputData1.Text = distances[0].ToString();
+                OutputData2.Text = distances[1].ToString();
+                OutputData3.Text = distances[2].ToString();
+                OutputData4.Text = distances[3].ToString();
+                OutputData5.Text = distances[4].ToString();
+
+                i = 0;
+
+            }
+
+        }
+
+        void TranslateInstance()
+        {
+            // Create a new Linear kernel
+            IKernel kernel = new Linear();
+
+            // Create a new Multi-class Support Vector Machine with one input,
+            //  using the linear kernel and for four disjoint classes.
+            var machine = new MulticlassSupportVectorMachine(5, kernel, numOfClasses);
+
+            // Create the Multi-class learning algorithm for the machine
+            var teacher = new MulticlassSupportVectorLearning(machine, inputs, outputs);
+
+            // Configure the learning algorithm to use SMO to train the
+            //  underlying SVMs in each of the binary class subproblems.
+            teacher.Algorithm = (svm, classInputs, classOutputs, i, j) =>
+                 new SequentialMinimalOptimization(svm, classInputs, classOutputs);
+
+            // Run the learning algorithm   
+            double error = teacher.Run(); // output should be 0
+
+            double[] distances = new double[5];
+            distances = LeapEventListener.getDistances(currentFrame);
+
+            int decision = machine.Compute(distances); //svm AI 
+            output.Text = output.Text + Char2SvmClass.class2svm(decision);
+        }
+
+        private void InitializeSVM()
+        {
+            // ============================================== Database ==============================================
+            connection = new OleDbConnection();
+            command = new OleDbCommand();
+            double thumb, index, middle, ring, pinky;
+
+            int svmClass = 0;
+
+            connection.ConnectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\SVMdataset.accdb";
+            command.Connection = connection;
+
+            string q = "select * from DataSet";
+            command.CommandText = q;
+            connection.Open();
+            dr = command.ExecuteReader();
+            int k = 0;
+            DataTable dt = new DataTable();
+            dt.Load(dr);
+            outputs = new int[dt.Rows.Count];
+            inputs = new double[dt.Rows.Count][];
+            dr = command.ExecuteReader();
+
+            if (dr.HasRows) // run over the table and put it on the list
+            {
+                while (dr.Read())
+                {
+                    char c = Convert.ToChar(dr[1]);
+
+
+                    svmClass = Char2SvmClass.char2class(c);//static method that converts the char to a class ;
+
+                    thumb = Convert.ToDouble(dr[2]);
+                    index = Convert.ToDouble(dr[3]);
+                    middle = Convert.ToDouble(dr[4]);
+                    ring = Convert.ToDouble(dr[5]);
+                    pinky = Convert.ToDouble(dr[6]);
+
+                    inputs[k] = new double[] { thumb, index, middle, ring, pinky };
+                    outputs[k] = svmClass;
+
+
+                    k++;
+                }
+            }
+            connection.Close();
+
+
+
+
+            //get from DB the number of classes
+            command.Connection = connection;
+            q = "select Distinct DataSet.Letter from DataSet";
+            command.CommandText = q;
+            connection.Open();
+            dr = command.ExecuteReader();
+            dt = new DataTable();
+            dt.Load(dr);
+            numOfClasses = dt.Rows.Count;
+            connection.Close();
+
+
+
+        }
+
+
+        private void Timer_Tick(Object sender, EventArgs e) //timer for continuous check 
 		{
 
             ts++;
             if(ts%100==0)
             {
                
-            }
-			
-			
-			
-			
+            }						
 			
 		}
-        private void button3_Click(object sender, EventArgs e)
+        private void BackToMenuBtn_Click(object sender, EventArgs e)
         {
             MainMenu menu = new MainMenu();
             menu.Show();
